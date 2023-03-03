340ba8ad57fd33db2d49ae383874af5d2ab1cc07
ryansplan <62387770+ryansplan@users.noreply.github.com>
2023-03-03 14:24:23 -0500

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, secondNumber, firstNumber);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //In real world scenario, we would want to log this message.
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
